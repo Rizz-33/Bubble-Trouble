b186e670aa4a568994a98004bf5c62c9de0c8b6a
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-11 20:07:48 +0530

    modify: button

--- lib/button.dart
+++ lib/button.dart
@@ -5,6 +5,11 @@ class MyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      color: Colors.brown,
+      width: 50,
+      height: 50,
+      child: Text("data"),
+    );
   }
 }
