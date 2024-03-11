44cb712267a51227c72cc9ff32af0ce055bb0ed9
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-11 20:14:22 +0530

    modify: constructor

--- lib/button.dart
+++ lib/button.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+  final icon;
+  
+  const MyButton({this.icon});
 
   @override
   Widget build(BuildContext context) {
