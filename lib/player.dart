16bbac788228d8bc7f67c023b7f861c3df7b1da4
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-12 11:58:19 +0530

    modify: home

--- lib/player.dart
+++ lib/player.dart
@@ -12,7 +12,7 @@ class MyPlayer extends StatelessWidget {
       child: ClipRRect(
         borderRadius: BorderRadius.circular(25),
         child: Image.asset(
-          'lib/images/mushroom.png',
+          'assets/images/mushroom.png', // Update the path accordingly
           height: 50,
           width: 50,
           fit: BoxFit.fill,
