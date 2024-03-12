614eb8fc99dee43b91f2cf4563efaf3748ba2bff
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-12 09:16:11 +0530

    modify: ball alignment

--- lib/ball.dart
+++ lib/ball.dart
@@ -9,11 +9,14 @@ class MyBall extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 10,
-      height: 10,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.black,
+      alignment: Alignment(bX, bY),
+      child: Container(
+        width: 20,
+        height: 20,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.black,
+        ),
       ),
     );
   }
