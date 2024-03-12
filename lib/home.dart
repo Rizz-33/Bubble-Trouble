d70c8c541084d1eb418aa05bb1527340ef690e74
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-12 10:43:36 +0530

    modify: player out

--- lib/home.dart
+++ lib/home.dart
@@ -120,7 +120,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   bool playerOut() {
-    if ((bX - X).abs() < 0.05) {
+    if ((bX - X).abs() < 0.05 && bY > 0.95) {
       return true;
     } else {
       return false;
