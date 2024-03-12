6e6a271b276f6663aacb54e14aab0f55ede01b36
Rizz-33 <risiniamarathunga@gmail.com>
2024-03-12 10:01:42 +0530

    feat: check missile hit ball

--- lib/home.dart
+++ lib/home.dart
@@ -83,6 +83,11 @@ class _HomePageState extends State<HomePage> {
           timer.cancel();
           midShot = false;
         }
+        if (bY > heightToCoordinate(mH)  && (bX - mX).abs() < 0.3){
+          resetMissile();
+          bY = 5;
+          timer.cancel();
+        }
       });
     }
   }
